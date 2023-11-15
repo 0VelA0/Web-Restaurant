70ccc0d5ba47ffb3aeef3da5b652716ba23a0e7a
Carlos Martin <charlymape@outlook.com>
2023-11-15 02:21:15 -0600

    Actualizacion del proyecto
    Adición de nuevos paquetes, funciones en package.json, constantes en diaryServices, creación de nuevo archivo para typado de entidades para usuarios.json, uso de comando .get en el router para conseguir usuarios por medio del url.

--- express-typescript/srs/services/diaryServices.ts
+++ express-typescript/srs/services/diaryServices.ts
@@ -1,4 +1,7 @@
+import { UsuarioEntry } from '../types'
 import diaryData from './usuarios.json'
 
-export const getEntries = () => diaryData
-export const addEntry = () => null
+const usuarios: UsuarioEntry[] = diaryData as UsuarioEntry[]
+
+export const getEntries = (): UsuarioEntry[] => usuarios
+export const addEntry = (): undefined => undefined
---
+++ express-typescript/srs/types.d.ts
@@ -0,0 +1,17 @@
+export type generos = 'Hombre' | 'Mujer' 
+// export type edades = `${number}${number}`
+// export type fechatiempoexacto = Date() 
+
+export interface UsuarioEntry{
+    id: number
+    nombre: string
+    genero: generos
+    edad: number
+    correoelectronico: string
+    contraseña: string
+    fechadecreacion: string
+    usuariodecreacion: string
+    fechadeactualizacion: string
+    usuariodeactualizacion: string
+    activo: boolean
+}
